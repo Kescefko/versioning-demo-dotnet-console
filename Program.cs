fd4d813d3131a749bb4aabfd645edd178fdf1ea4
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-01-29 11:58:43 +0100

    add dotnet console app for versioning

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VersioningDemo
+{
+    class Program
+    {
+        // The version number will be manually set based on the versioning strategy
+        static string version = "v0.0.0";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Azure DevOps!");
+            Console.WriteLine($"Version: {version}");
+        }
+    }
+}
